4a9233b81931bde0cfe07501406eec387f64b2ef
Nikki Nicholas Romero <nikkinicholas.romero@gmail.com>
2020-07-04 11:23:08 -0400

    Added unit test

---
+++ src/test/java/com/demo/controller/MainControllerTest.java
@@ -0,0 +1,28 @@
+package com.demo.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("dev")
+@WebMvcTest(MainController.class)
+public class MainControllerTest {
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void main() throws Exception {
+        this.mockMvc.perform(get("/"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("Hello Devs")));
+    }
+}
